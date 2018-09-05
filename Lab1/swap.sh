0a38f9185927f7672f0c1308c6fd15112e1f54b6
Aron Hrafnsson <aronhr@users.noreply.github.com>
2018-09-05 15:19:54 +0000

    Update swap.sh

--- Lab1/swap.sh
+++ Lab1/swap.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
+swap()
+{
+  local TMPFILE=tmp.$$
+  mv "$1" $TMPFILE
+  mv "$2" "$1"
+  mv $TMPFILE "$2"
+}
 
 if [ $# != 2 ]; then
   echo "Invalid number of arguments."
   exit
-fi
-
-if [[ -f "$1" && -f "$2" ]]; then
-  echo "Both files exist"
 else
-   echo "One or both of the files do not exist"
-   exit
-fi
-
-if [[ -r "$1" && -r "$2" ]];
-then
-  echo "Both files are readable"
-else
-  echo "One or both of the files are not readable"
-  exit
-fi
-
-if [[ -w "$1" && -w "$2" ]];
-then
-  echo "Both files are writable"
-else
-  echo "One or both of the files are not writeable"
-  exit
+  if [[ -f "$1" && -f "$2" ]]; then
+    echo "Both files exist"
+    if [[ -r "$1" && -r "$2" ]];
+    then
+      echo "Both files are readable"
+      if [[ -w "$1" && -w "$2" ]];
+      then
+        echo "Both files are writable"
+        swap "$1" "$2"
+      else
+        echo "One or both of the files are not writeable"
+        exit
+      fi
+    else
+      echo "One or both of the files are not readable"
+      exit
+    fi
+  else
+     echo "One or both of the files do not exist"
+     exit
+  fi
 fi
