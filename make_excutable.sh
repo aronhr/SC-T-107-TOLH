fc8742930835b65059a9db8f1e881edd33cbb0d1
Aron Hrafnsson <aronhr@users.noreply.github.com>
2018-09-05 14:02:10 +0000

    Create make_excutable.sh

---
+++ make_excutable.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ $# -gt 1 ]; then
+  echo "Invalid number of arguments"
+  exit
+fi
+
+if [ $# -eq 0 ]; then
+  LINK_OR_DIR="."
+else
+  LINK_OR_DIR="$1"
+fi
+
+if [ -d "$LINK_OR_DIR" ]; then
+  if [ -L "$LINK_OR_DIR" ]; then
+    echo "The directory provided is a symlink"
+  else
+    echo "The directory provided is a solid directory!"
+  fi
+else
