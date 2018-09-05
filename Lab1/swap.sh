36efc4772c6901f3d2534a0a2ebbf019e8901758
Aron Hrafnsson <aronhr@users.noreply.github.com>
2018-09-05 15:26:57 +0000

    Update swap.sh
    Remove unnecessary echo's

--- Lab1/swap.sh
+++ Lab1/swap.sh
@@ -12,13 +12,10 @@ if [ $# != 2 ]; then
   exit
 else
   if [[ -f "$1" && -f "$2" ]]; then
-    echo "Both files exist"
     if [[ -r "$1" && -r "$2" ]];
     then
-      echo "Both files are readable"
       if [[ -w "$1" && -w "$2" ]];
       then
-        echo "Both files are writable"
         swap "$1" "$2"
       else
         echo "One or both of the files are not writeable"
